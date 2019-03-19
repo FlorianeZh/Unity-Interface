bc9f1d809c98b5e67864394345ece53fbdffbf5f
Floriane <floriane.zhang@imt-atlantique.net>
2019-03-19 16:43:16 +0800

    Interface for Virtual Reality project

---
+++ Menu-2bis/Assets/Assets/Scripts/Manager2.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Manager2 : MonoBehaviour {
+	
+	public Slider mSlider;
+	public Slider rotationSlider;
+	public GameObject boutonModifierTaille;
+
+
+	public Transform canvas;
+	public Transform UIPanel; //Will assign our panel to this variable so we can enable/disable it
+	public Transform PanelDimension;
+	public Transform PanelRotation;
+	bool isPaused; //Used to determine paused state
+
+	RaycastHit hit;
+	Ray ray;
+	GameObject cube;
+	Vector3 initialScale;
+
+
+	void Start()
+	{
+		
+		isPaused = false; //make sure isPaused is always false when our scene opens
+		UIPanel.gameObject.SetActive(false); //make sure our pause menu is disabled when scene starts
+		PanelDimension.gameObject.SetActive(false);
+		PanelRotation.gameObject.SetActive(false);
+
+	}
+		
+
+	void Update()
+	{
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		//an object is selected by the user with its mouse, the game is paused
+		if (Physics.Raycast (ray, out hit) && Input.GetMouseButtonDown (0) && !isPaused) {
+			cube = hit.collider.gameObject;
+			pause ();
+		} 
+	}
+		
+
+	/* pause() pauses the game and turn on the object interface */
+	public void pause()
+	{	
+		isPaused = true;
+		setPositionPanel();
+		UIPanel.gameObject.SetActive(true); 
+		Time.timeScale = 0f; 
+	}
+
+
+	/* unPause() resumes the game*/ 
+	public void unPause()
+	{	
+		isPaused = false;
+		UIPanel.gameObject.SetActive(false);
+		Time.timeScale = 1f; 
+
+	}
+
+
+	/* set the position of the panel above the selected object */
+	private void setPositionPanel(){
+		float height = cube.GetComponent<Collider> ().bounds.size.y;
+		Debug.Log (height);
+		Vector3 nouvellePosition = new Vector3 (cube.transform.position.x, cube.transform.position.y + height, cube.transform.position.z);
+		canvas.transform.position = nouvellePosition;
+
+	}
+		
+  	
+
+	/* resize() changes the size of the selected object */
+	public void resize(){
+		cube.transform.localScale = (mSlider.value)*(Vector3.one);
+	}
+		
+	/* resizePanel() displays the resize panel*/ 
+	public void resizePanel(){
+		UIPanel.gameObject.SetActive(false);
+		PanelDimension.gameObject.SetActive(true);
+	}
+
+
+
+	/* delete() deletes the selected object and closes the interface */
+	public void delete(){
+		Destroy(cube); 
+		unPause ();
+	}
+
+
+
+	/* rotatePanel() displays rotation panel */
+	public void rotatePanel(){
+		UIPanel.gameObject.SetActive(false);
+		PanelRotation.gameObject.SetActive(true);
+	}
+
+
+	/* rotate() rotates the object with a slider */
+	public void rotate(){
+		Vector3 tourner = (-rotationSlider.value) * (Vector3.up);
+		cube.transform.rotation = Quaternion.Euler(0, rotationSlider.value, 0);
+	}
+
+
+
+	/* changeColor() changes the color of the selected object and color it with a random color */
+	public void changeColor() {
+		// Find a random color
+		float red = Random.Range(0f,1f);
+		float green = Random.Range(0f,1f);
+		float blue = Random.Range(0f,1f);
+		Color newColor = new Color( red, green, blue );
+
+		// Apply color
+		cube.GetComponent<MeshRenderer>().material.color = newColor;
+
+		if (mSlider.gameObject.activeSelf) {
+			mSlider.gameObject.SetActive (false);
+			boutonModifierTaille.SetActive (true);
+		}
+
+	}
+
+
+	public void closeInterface(){
+		if (isPaused) {
+			unPause();
+		}
+	}
+
+
+	/* closeResize() closes the resize Panel */
+	public void closeResize(){
+		UIPanel.gameObject.SetActive(true);
+		PanelDimension.gameObject.SetActive(false);
+	}
+
+
+	/* closeRotate() closes the rotate Panel */
+	public void closeRotate(){
+		UIPanel.gameObject.SetActive(true);
+		PanelRotation.gameObject.SetActive(false);
+
+	}
+		
+
+}
---
+++ Menu-2bis/Assets/Assets/Scripts/ManagerSwipe.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManagerSwipe : MonoBehaviour {
+
+		public GameObject trailPrefab;
+		GameObject thisTrail;
+		Vector3 startPos;
+		Plane objPlane;
+
+		void Start()
+		{
+			objPlane = new Plane(Camera.main.transform.forward*-1, this.transform.position);
+		}
+
+
+		void Update () 
+		{
+			if((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) ||
+				Input.GetMouseButtonDown(0))
+			{
+				thisTrail = (GameObject) Instantiate(trailPrefab, 
+					this.transform.position, 
+					Quaternion.identity);
+				Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+				float rayDistance;
+				if (objPlane.Raycast(mRay, out rayDistance))
+					startPos = mRay.GetPoint(rayDistance);
+			}
+			else if(((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) 
+				|| Input.GetMouseButton(0)))
+			{
+
+				Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+				float rayDistance;
+				if (objPlane.Raycast(mRay, out rayDistance))
+					thisTrail.transform.position = mRay.GetPoint(rayDistance);
+			}
+			else if((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) ||
+				Input.GetMouseButtonUp(0))
+			{
+				if(Vector3.Distance(thisTrail.transform.position, startPos) < 0.1)
+					Destroy(thisTrail);
+			}
+		}
+}
+	
